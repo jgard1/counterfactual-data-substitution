3876b693d0d4a6a5afb57acb087078555e7c60b9
dalelee02 <dale.lee@ev.energy>
2021-04-29 23:32:39 -0700

    sh

--- src/preprocess.sh
+++ src/preprocess.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python3 process_corpus.py invert_word_names ./counterfactual-data-substitution/tagged_wikicorpus ./counterfactual-data-substitution/invert_names/
-python3 process_corpus.py invert_word_neutral ./counterfactual-data-substitution/tagged_wikicorpus ./counterfactual-data-substitution/invert_neutral/
-python3 process_corpus.py invert_control ./counterfactual-data-substitution/tagged_wikicorpus ./counterfactual-data-substitution/invert_control/
+python3 process_corpus.py invert_word_names ~/counterfactual-data-substitution/tagged_wikicorpus/ ~/counterfactual-data-substitution/invert_names/
+python3 process_corpus.py invert_word_neutral ~/counterfactual-data-substitution/tagged_wikicorpus/ ~/counterfactual-data-substitution/invert_neutral/
+python3 process_corpus.py invert_control ~/counterfactual-data-substitution/tagged_wikicorpus/ ~/counterfactual-data-substitution/invert_control/
